aa140d2570c578aa33f0e74b21e740abbc839fc6
Roman Annenkov <roman.annenkov@gmail.com>
2024-02-26 20:23:34 +0300

    added project for hw seminar one with task

--- hwSeminarOne/src/main/java/Main.java
+++ hwSeminarOne/src/main/java/Main.java
@@ -1,2 +1,7 @@
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("hw seminar one");
+        //Напишите программу, которая использует Stream API для обработки списка чисел.
+        //Программа должна вывести на экран среднее значение всех четных чисел в списке.
+    }
 }
